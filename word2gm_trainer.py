16f4de70c5eef5e78bda412807e7d2269c215628
Lukas Prediger <lukas.prediger@rtwh-aachen.de>
2017-12-03 23:04:19 +0900

    Fixed save_vocab() for python 3.

--- word2gm_trainer.py
+++ word2gm_trainer.py
@@ -492,7 +492,7 @@ class Word2GMtrainer(object):
     opts = self._options
     with open(os.path.join(opts.save_path, "vocab.txt"), "w") as f:
       for i in range(opts.vocab_size):
-        vocab_word = tf.compat.as_text(opts.vocab_words[i]).encode("utf-8")
+        vocab_word = tf.compat.as_text(opts.vocab_words[i])#.encode("utf-8")
         f.write("%s %d\n" % (vocab_word,
                              opts.vocab_counts[i]))
 
